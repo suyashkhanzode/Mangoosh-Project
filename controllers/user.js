c263879bd9dce4f193d96ce9e46bef012635129e
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-07-02 19:16:16 +0530

    User Table

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,18 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-exports.addUser = async (req,res,next) =>{
-    const {name,email,password} = req.body;
-    const u =  new User(name,email,password);
-    const result = await u.save();
-    res.status(201).json({message:result});
-}
+exports.addUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-exports.findUserById = async (req,res,next) =>{
-    const userId = req.params.userId;
-    const user = await User.findByUserId(userId);
-    res.status(200).json({result : user});
-}
+  const result = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+  res.status(201).json({ message: result });
+};
+
+exports.findUserById = async (req, res, next) => {
+  const userId = req.params.userId;
+  const user = await User.findById(userId)
+  res.status(200).json({ result: user });
+};
